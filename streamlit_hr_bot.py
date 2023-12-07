78d7adb31ab382f6ec88ae2d47af7adb71e20935
Jack Ogozaly <72467438+JackOgozaly@users.noreply.github.com>
2023-12-07 15:13:14 -0500

    Update streamlit_hr_bot.py

--- streamlit_hr_bot.py
+++ streamlit_hr_bot.py
@@ -30,8 +30,7 @@ st.title('USDA HR Document Explorer')
 st.sidebar.image('USDA_logo.png', width=100)
 st.caption('A LLM interface to explore various MRP HR Documents')
 #Introduction text
-introduction_text = """Hello! I'm the digital version of Jack. I'm trapped in here and will just tell you HR information. 
-In a way, I'm the most accurate replica of real-Jack. I very well may be Jack."""
+introduction_text = """Hello! I can help you find relevant HR documents and answer various HR related questions around Applicant Flow Data, USA Staffing, and NFC Insight data systems. How can I help you?"""
 
 #Bring in API Key
 os.environ["OPENAI_API_KEY"] = st.secrets["openai_key"]
