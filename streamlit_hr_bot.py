9655e27cb7351312e092c3641bc044f07856b554
Jack Ogozaly <72467438+JackOgozaly@users.noreply.github.com>
2023-12-07 15:07:36 -0500

    Update streamlit_hr_bot.py

--- streamlit_hr_bot.py
+++ streamlit_hr_bot.py
@@ -27,7 +27,7 @@ import sqlite3
 
 #Streamlit customization items
 st.title('🐄 Jack Bot 🐄')
-st.sidebar.image('napoleon_painting.jpg')
+st.sidebar.image('USDA_logo.png')
 st.caption('A LLM interface to explore various MRP HR Documents')
 #Introduction text
 introduction_text = """Hello! I'm the digital version of Jack. I'm trapped in here and will just tell you HR information. 
